5a366cd09d92457a434d75ebfd9434afa693467b
auglish-cq <auglish@caraquri.com>
2017-06-09 10:45:07 +0000

    moidfy restart script

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps aux | grep uwsgi | grep -v grep | awk '{print "kill -9", $2}' | sh
-uwsgi --ini ~/book_manager_server/setting/uwsgi.ini &
+cd /home/ec2-user/book_manager_server && uwsgi --ini /home/ec2-user/book_manager_server/setting/uwsgi.ini &
